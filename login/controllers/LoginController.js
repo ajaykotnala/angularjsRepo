5965329bb416b918d212f46798e71e14fefa699e
unknown <ajay.kotnalajpr@gmail.com>
2015-11-30 21:43:01 +0530

    first push for login UI

---
+++ login/controllers/LoginController.js
@@ -0,0 +1,25 @@
+﻿'use strict';
+
+app.controller('LoginController', function ($scope, LoginService) {
+    $scope.services = [];
+    $scope.IsHide = true;
+    //$scope.totalRecordsCount = 10;
+    function ShowPassword() {
+        var key_attr = $('#key').attr('type');
+        if (key_attr != 'text') {
+            $('.checkbox').addClass('show');
+            $('#key').attr('type', 'text');
+        } else {
+            $('.checkbox').removeClass('show');
+            $('#key').attr('type', 'password');
+        }
+    }
+
+    $scope.private = {
+        ShowPassword: ShowPassword
+    };
+    function init() {
+        // getAllServices();
+    }
+    init();
+});
---
+++ login/services/LoginService.js
@@ -0,0 +1,23 @@
+﻿app.factory('LoginService', function ($http, $q) {
+
+    //need to find a way to automate urls.
+    //localization needs to be3 implemented here
+    var ServicePluggerUrl = 'https://493nedp085.execute-api.eu-west-1.amazonaws.com/Beta/servicepluggers';
+
+    function getAllServices() {
+        var deferred = $q.defer();
+        $http.get(ServicePluggerUrl, { cache: true })
+            .success(function (response) {
+                deferred.resolve(response);
+            }).error(function (error) {
+                //notify service need to inject here.
+                deferred.reject(error);
+            });
+        return deferred.promise;
+    }
+
+    return {
+        getAllServices: getAllServices
+    };
+
+});
